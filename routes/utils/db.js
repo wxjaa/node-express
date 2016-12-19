68045facfcb87b36c2784352287ba746d26c0080
wxjaa <980607279@qq.com>
2016-12-19 16:30:45 +0800

    6

--- routes/utils/db.js
+++ routes/utils/db.js
@@ -22,11 +22,11 @@ var fs = require('fs');
 //var uuid = require("node-uuid");
 var sequelize = new Sequelize(
     "premissionManagement", // 数据库名
-    "yunfei",   // 用户名
-    "123456",   // 用户密码
+    "",   // 用户名
+    "",   // 用户密码
     {
-        "dialect": "mysql",  // 数据库使用mysql
-        "host": "84mall.cn", // 数据库服务器ip
+        "dialect": "",  // 数据库使用mysql
+        "host": "", // 数据库服务器ip
         "port": 3306,        // 数据库服务器端口
         "define": {
             // 字段以下划线（_）来分割（默认是驼峰命名风格）
